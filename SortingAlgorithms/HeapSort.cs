3340ea95282d0a7727ec851b290ca80bbacfb1ad
Charansaimsc <charanmsc7969@gmail.com>
2026-01-06 15:36:13 +0530

    [Charan] Add.Main Method Sorting algorithms Solution

--- SortingAlgorithms/HeapSort.cs
+++ SortingAlgorithms/HeapSort.cs
@@ -1,4 +1,12 @@
-﻿using System;
+﻿/*Problem Statement:
+A school collects students’ ages (ranging from 10 to 18) and wants them sorted. Implement
+Counting Sort in C# for this task.
+Hint:
+● Create a count array to store the frequency of each age.
+● Compute cumulative frequencies to determine positions.
+● Place elements in their correct positions in the output array*/
+
+using System;
 
 public class HeapSorter
 {
--- SortingAlgorithms/Program.cs
+++ SortingAlgorithms/Program.cs
@@ -1,2 +1,50 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+
+using System;
+namespace Sorting
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] marks = { 12, 3, 99, 77, 55, 33, 100 };
+            BubbleSort BSort = new BubbleSort(marks);
+            BSort.SortMarks();
+            BSort.Display();
+
+
+            int[] EmployeeId = { 101, 990, 888, 222, 001 };
+            InsertionSort IS = new InsertionSort(EmployeeId);
+            IS.Sort();
+            IS.Display();
+
+
+            int[] BookPrices = { 1009, 999, 888, 444, 1003, 33 };
+            MergeSorter MS = new MergeSorter(BookPrices);
+            MS.Sort();
+            MS.Display();
+
+            int[] ProductPrices = {11,66,4,4,366,776,2,44};
+            QuickSorter QS = new QuickSorter(ProductPrices);
+            QS.Sort();
+            QS.Display();
+
+
+            int[] ExamScores = { 99, 99, 98, 77, 66, 88, 99, 11 };
+            SelectionSort SS = new SelectionSort(ExamScores);
+            SS.Sort();
+            SS.Display();
+
+            int[] ExpectedSalary = { 13333, 233232, 454545, 435455, 9034 };
+            HeapSorter HS = new HeapSorter(ExpectedSalary);
+            HS.Sort();
+            HS.Display();
+
+
+            int[] studentAges = { 15, 12, 18, 10, 14, 12, 16, 15 };
+            CountingSorter CS = new CountingSorter(studentAges);
+            CS.Sort();
+            CS.Display();
+        }
+    }
+}
