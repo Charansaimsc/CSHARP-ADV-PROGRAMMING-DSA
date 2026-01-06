2a2a82cd5d7a2af46c38611898096c17af61a7b6
Charansaimsc <charanmsc7969@gmail.com>
2026-01-06 21:47:53 +0530

    [charan] Add.ConcatenateStrings Solution

---
+++ FileHandling/ConcatenateStrings.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Text;
+public class ConcatenateString
+{
+	private string[] _strings;
+	public ConcatenateString(string[]  _strings)
+	{
+		this._strings = _strings;
+	}
+	public string Concatenate()
+	{
+		StringBuilder Sb = new StringBuilder();
+		for (int i = 0; i < _strings.Length; i++)
+		{
+			Sb.Append(_strings[i]);
+		}
+		return Sb.ToString();
+
+	}
+}
