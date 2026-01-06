6bace20aa1c2c5f584870eb32213f26359f4f3fe
Charansaimsc <charanmsc7969@gmail.com>
2026-01-06 14:32:23 +0530

    [charan] Add.INsertionSort Solution

---
+++ SortingAlgorithms/InsertionSort.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+public class InsertionSort
+{
+	private int[] EmployeeId;
+
+	public InsertionSort(int[] EmployeeId)
+	{
+		this.EmployeeId = EmployeeId;
+	}
+	public void Sort()
+	{
+		for (int i = 1; i < EmployeeId.Length; i++)
+		{
+			int key = EmployeeId[i];
+			int j = i - 1;
+			while (j >= 0 && EmployeeId[j] > key)
+			{
+				EmployeeId[j + 1] = EmployeeId[j];
+				j--;
+			}
+            EmployeeId[j + 1] = key;
+
+		}
+	}
+	public void Display()
+	{
+		for(int i = 1; i < EmployeeId.Length; i++)
+		{
+			Console.WriteLine(EmployeeId[i]);
+		}
+	}
+}
