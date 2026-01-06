c90df226e205b2eba1b9f5f6602a6ac365f2c0c3
Charansaimsc <charanmsc7969@gmail.com>
2026-01-06 21:31:51 +0530

    [charan] Add. RemoveDuplicates using Stringbuilder

---
+++ FileHandling/RemoveDuplicates.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Text;
+public class RemoveDuplicatesUsingStringBuilder
+{
+	private string word;
+	public RemoveDuplicatesUsingStringBuilder(string word)
+	{
+		this.word = word; 
+	}
+	public string removeDuplicates()
+	{
+		StringBuilder Sb = new StringBuilder();
+		for (int i = 0; i < word.Length; i++)
+		{
+			if (!Sb.ToString().Contains(word[i]))
+			{
+				Sb.Append(word[i]);
+			}
+		}
+		return Sb.ToString();
+	}
+}
