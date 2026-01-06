bf43f40dfa900fac78ca7054b538fdfb7944b4cf
Charansaimsc <charanmsc7969@gmail.com>
2026-01-06 20:59:36 +0530

    [charan] Add.Reverse String using StringBuilder Solution

---
+++ FileHandling/StringReverse.cs
@@ -0,0 +1,23 @@
+﻿/*Problem: Write a program that uses StringBuilder to reverse a given string. For example, if
+the input is "hello", the output should be "olleh".*/
+using System;
+using System.Text;
+public class ReverseString
+{
+	private string _value;
+	public ReverseString(string _value)
+	{
+		this._value = _value; 
+	}
+	public string Reverse()
+	{
+		StringBuilder sb = new StringBuilder();
+		
+			for (int i = _value.Length - 1; i >= 0; i--)
+			{
+		
+				sb.Append(_value[i]);
+			}
+			return sb.ToString();
+	}
+}
