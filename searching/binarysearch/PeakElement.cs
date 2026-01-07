622d735d7b80dd98b3be900acf74b4c6effaa3d3
Charansaimsc <charanmsc7969@gmail.com>
2026-01-07 15:21:01 +0530

    [charan] Add.Peak element Problem

---
+++ searching/binarysearch/PeakElement.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+public class PeakElementFinder
+{
+    private readonly int[] array;
+
+    public PeakElementFinder(int[] array)
+    {
+        this.array = array;
+    }
+
+    public int FindPeakIndex()
+    {
+        int low = 0;
+        int high = array.Length - 1;
+
+        while (low < high)
+        {
+            int mid = low + (high - low) / 2;
+
+            if (array[mid] < array[mid + 1])
+            {
+                low = mid + 1;
+            }
+            else
+            {
+                high = mid;
+            }
+        }
+        return low;
+    }
+}
