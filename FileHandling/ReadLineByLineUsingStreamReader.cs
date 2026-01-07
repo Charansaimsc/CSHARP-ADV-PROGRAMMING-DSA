8228ffc8a984ef88216a2da21b27602d64bba8d0
Charansaimsc <charanmsc7969@gmail.com>
2026-01-07 11:06:23 +0530

    [charan] Add.Solution using Stream Reader

---
+++ FileHandling/ReadLineByLineUsingStreamReader.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+public class FileLineReader
+{
+	private string FilePath;
+	public FileLineReader(string FilePath)
+	{
+		this.FilePath = FilePath;
+	}
+	public void ReadLineByLine()
+	{
+		try
+		{
+			using(StreamReader SR = new StreamReader(FilePath))
+			{
+				string line;
+				while ((line = SR.ReadLine()) != null)
+				{
+					Console.WriteLine(line);
+				}
+			}
+		}
+		catch (Exception e)
+		{
+			Console.WriteLine(e.Message);
+		}
+	}
+}
