6e9edf7fb2d0fc6ef28a5350f3e49c670c1fd8fd
Charansaimsc <charanmsc7969@gmail.com>
2026-01-07 12:29:11 +0530

    [charan] Add.Code to bINARYdATA To CharCTERS

---
+++ FileHandling/ByteStreamToCharStream.cs
@@ -0,0 +1,34 @@
+﻿/* Convert Byte Stream to Character Stream Using StreamReader
+Problem: Write a program that uses StreamReader to read binary data from a file and print
+it as characters.
+*/
+using System;
+using System.IO;
+using System.Text;
+
+public class ByteToChar
+{
+	private readonly string FilePath;
+	public ByteToChar(string FilePath)
+	{
+		this.FilePath = FilePath;
+	}
+	public void ByteToCharacter()
+	{
+		try
+		{
+			using(FileStream FS = new FileStream(FilePath, FileMode.Open, FileAccess.Read)) 
+			{ 
+				using(StreamReader SR= new StreamReader(FS,Encoding.UTF8))
+				{
+						string content = SR.ReadToEnd();
+					Console.WriteLine(content);
+						
+				}
+			}
+		}catch (Exception e)
+		{
+			Console.WriteLine(e.Message);
+		}
+	}
+}
