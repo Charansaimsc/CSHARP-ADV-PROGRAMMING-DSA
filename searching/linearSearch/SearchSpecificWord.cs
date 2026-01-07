c08bfb4e17ca7b75428dd3461cbc6c264b96f65c
Charansaimsc <charanmsc7969@gmail.com>
2026-01-07 14:48:34 +0530

    [charan] Add. Search for a specific word in a sentence

---
+++ searching/linearSearch/SearchSpecificWord.cs
@@ -0,0 +1,28 @@
+﻿/*Search for a Specific Word in a List of Sentences
+Problem: You are given an array of sentences. Write a program that performs Linear Search
+to find the first sentence containing a specific word.
+*/
+using System;
+
+public class SentenceSearch
+{
+    private readonly string[] sentences;
+
+    public SentenceSearch(string[] sentences)
+    {
+        this.sentences = sentences;
+    }
+
+    public string FindSentenceContainingWord(string word)
+    {
+        for (int i = 0; i < sentences.Length; i++)
+        {
+            if (sentences[i].Contains(word, StringComparison.OrdinalIgnoreCase))
+            {
+                return sentences[i]; 
+            }
+        }
+
+        return null; 
+    }
+}
