aff6d40d5d1ff7f3bea8c0958466dd233d8d0f90
Charansaimsc <charanmsc7969@gmail.com>
2026-01-07 14:33:19 +0530

    [charan] Add.FirstNegativeNumber in an arrey problem

---
+++ searching/linearSearch/FirstNegativeNumber.cs
@@ -0,0 +1,27 @@
+﻿/* You are given an integer array. Write a program that performs Linear Search to find
+the first negative number in the array.
+*/
+using System;
+
+public class FirstNegativeNumber
+{
+    private int[] numbers;
+
+    public FirstNegativeNumber(int[] numbers)
+    {
+        this.numbers = numbers;
+    }
+
+    public int FindNegative()
+    {
+        for (int i = 0; i < numbers.Length; i++)
+        {
+            if (numbers[i] < 0)
+            {
+                return numbers[i]; 
+            }
+        }
+
+        return 0; 
+    }
+}
---
+++ searching/linearSearch/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+namespace Searching
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = { 1, 4, 5, -8, 44, 3, 2 };
+            FirstNegativeNumber FN = new FirstNegativeNumber(numbers);
+            Console.WriteLine(FN.FindNegative());
+        }
+    }
+}
