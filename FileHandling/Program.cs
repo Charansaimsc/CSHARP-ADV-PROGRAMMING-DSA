dbad2a55e8f010b074838d267ed8643bec284240
Charansaimsc <charanmsc7969@gmail.com>
2026-01-07 12:37:41 +0530

    [charan] Add.Main method for all file handling files

--- FileHandling/Program.cs
+++ FileHandling/Program.cs
@@ -1,2 +1,35 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+namespace Filehandling
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            //ReverseString RS = new ReverseString("charan");
+            //Console.WriteLine(RS.Reverse());
+
+            //RemoveDuplicatesUsingStringBuilder RD = new RemoveDuplicatesUsingStringBuilder("CHARAN");
+            //Console.WriteLine(RD.removeDuplicates());
+
+            //string[] _strings = { "hello", "charan", "sai" };
+            //ConcatenateString CS = new ConcatenateString(_strings);
+            //    Console.WriteLine(CS.Concatenate());
+
+
+            //StringPerformanceComparision SPC = new StringPerformanceComparision(10000000));
+            //SPC.Display();
+
+            //FileLineReader FR = new FileLineReader("sample.txt");
+            //FR.ReadLineByLine();
+
+            //CountOccurance CO = new CountOccurance("sample.txt","line");
+            //Console.WriteLine(CO.StringCounter());
+
+            //ByteToChar BC = new ByteToChar("sample.txt");
+            //BC.ByteToCharacter();
+
+            UserInputFileWriter writer = new UserInputFileWriter("userInput.txt");
+            writer.WriteInputToFile();
+        }
+    }
+}
---
+++ FileHandling/UserToFile.cs
@@ -0,0 +1,36 @@
+﻿/* Read User Input and Write to File Using StreamReader
+Problem: Write a program that reads user input from the console and writes it to a file.*/
+
+using System;
+using System.IO;
+
+public class UserInputFileWriter
+{
+    private readonly string filePath;
+
+    public UserInputFileWriter(string filePath)
+    {
+        this.filePath = filePath;
+    }
+
+    public void WriteInputToFile()
+    {
+        try
+        {
+            Console.WriteLine("Enter text to write into file:");
+            string userInput = Console.ReadLine();
+
+            using (StreamWriter writer = new StreamWriter(filePath, true))
+            {
+                writer.WriteLine(userInput);
+            }
+
+            Console.WriteLine("Input successfully written to file.");
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine("Error: " + ex.Message);
+        }
+    }
+}
+
