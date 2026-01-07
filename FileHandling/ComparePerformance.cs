f5635b0298d77de38fb537f9d75c1002711214a7
Charansaimsc <charanmsc7969@gmail.com>
2026-01-07 11:07:42 +0530

    [charan] Add.Performance comparision betweem string and StringBuilder

---
+++ FileHandling/ComparePerformance.cs
@@ -0,0 +1,44 @@
+﻿/* Compare StringBuilder Performance
+Problem: Write a program that compares the performance of StringBuilder for
+concatenating strings multiple times.*/
+using System;
+using System.Text;
+using System.Diagnostics;
+public class StringPerformanceComparision
+{
+    private int turns;
+	public StringPerformanceComparision(int turns)
+	{
+			this.turns = turns;
+	}
+
+    public long TestStringConcatenation()
+    {
+        Stopwatch stopwatch = Stopwatch.StartNew();
+
+        string result = "";
+        for (int i = 0; i < turns; i++)
+        {
+            result += "SaI";
+        }
+
+        stopwatch.Stop();
+        return stopwatch.ElapsedMilliseconds;
+    }
+    public long TestStringBuilderComparision()
+    {
+        Stopwatch stopwatch = Stopwatch.StartNew();
+        StringBuilder SB = new StringBuilder();
+        for (int i = 0; i < turns; i++)
+        {
+            SB.Append("sai");
+        }
+        SB.ToString();
+        stopwatch.Stop();
+        return stopwatch.ElapsedMilliseconds;
+    }
+    public void Display()
+    {
+        Console.WriteLine($"stringTimeTaken: {TestStringConcatenation()}" + $"StingBuilder TimeTaken: {TestStringBuilderComparision()}");
+    }
+}
