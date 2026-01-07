32f2cc972aadb42005764e490e6d41761a46e09d
Charansaimsc <charanmsc7969@gmail.com>
2026-01-07 15:11:13 +0530

    [charan] Add.Rotated sorted array problem

---
+++ searching/binarysearch/RotatedSortedArray.cs
@@ -0,0 +1,36 @@
+﻿/* You are given a rotated sorted array. Write a program that performs Binary Search
+to find the index of the smallest element in the array.
+*/
+using System;
+
+public class RotatedSortedArray
+{
+    private readonly int[] array;
+
+    public RotatedSortedArray(int[] array)
+    {
+        this.array = array;
+    }
+
+    public int FindSmallest()
+    {
+        int low = 0;
+        int high = array.Length - 1;
+
+        while (low < high)
+        {
+            int mid = low + (high - low) / 2;
+
+            if (array[mid] > array[high])
+            {
+                low = mid + 1;
+            }
+            else
+            {
+                high = mid;
+            }
+        }
+
+        return low; 
+    }
+}
