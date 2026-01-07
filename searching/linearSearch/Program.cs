02a9b846e47b27e512db86fbe368dee75d5f3e72
Charansaimsc <charanmsc7969@gmail.com>
2026-01-07 14:52:40 +0530

    [charan] Add.Main method

--- searching/linearSearch/Program.cs
+++ searching/linearSearch/Program.cs
@@ -5,9 +5,31 @@ namespace Searching
     {
         static void Main(string[] args)
         {
-            int[] numbers = { 1, 4, 5, -8, 44, 3, 2 };
-            FirstNegativeNumber FN = new FirstNegativeNumber(numbers);
-            Console.WriteLine(FN.FindNegative());
+            //int[] numbers = { 1, 4, 5, -8, 44, 3, 2 };
+            //FirstNegativeNumber FN = new FirstNegativeNumber(numbers);
+            //Console.WriteLine(FN.FindNegative());
+
+
+            string[] sentences =
+    {
+            "C# is a powerful language",
+            "Linear search is simple",
+            "StreamReader reads files",
+            "OOPS concepts are important"
+        };
+
+            SentenceSearch search = new SentenceSearch(sentences);
+            string result = search.FindSentenceContainingWord("search");
+
+            if (result != null)
+            {
+                Console.WriteLine("Sentence found:");
+                Console.WriteLine(result);
+            }
+            else
+            {
+                Console.WriteLine("Word not found in any sentence.");
+            }
         }
     }
 }
