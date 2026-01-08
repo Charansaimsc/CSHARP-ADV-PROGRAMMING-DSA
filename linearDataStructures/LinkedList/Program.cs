9f1c8e52cfcc8d5ddf6a2819e9c26ad61db3fd03
Charansaimsc <charanmsc7969@gmail.com>
2026-01-08 14:55:39 +0530

    [charan] Add.Main method problem

--- linearDataStructures/LinkedList/Program.cs
+++ linearDataStructures/LinkedList/Program.cs
@@ -1,2 +1,33 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+namespace lINKEDlIST
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            //NavigationHistory History = new BrowserHistory();
+            //History.Visit("land8form.com");
+            //History.Visit("BinaryBridge.com");
+            //History.Visit("Gla.ac.in");
+            //History.Visit("instagram.com");
+            //Console.WriteLine(History.Back());
+            //Console.WriteLine(History.Back());
+            //Console.WriteLine(History.Forward());
+            //Console.WriteLine(History.Current());
+
+
+            MediaPlayer player = new MusicPlayer();
+
+            player.AddMedia("Believer", "Imagine Dragons");
+            player.AddMedia("Numb", "Linkin Park");
+            player.AddMedia("Shape of You", "Ed Sheeran");
+
+            player.PlayNext();
+            player.PlayNext();
+            player.PlayNext();
+            player.PlayNext();
+
+            Console.ReadLine();
+        }
+    }
+}
