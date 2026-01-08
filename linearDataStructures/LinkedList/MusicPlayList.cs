e97461be57929502e34a402ada4d4ebfde99f692
Charansaimsc <charanmsc7969@gmail.com>
2026-01-08 14:38:22 +0530

    [charan] Add.Music Playlist problem

---
+++ linearDataStructures/LinkedList/MusicPlayList.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+
+using System;
+
+public class Song
+{
+    public string SongName { get; }
+    public string Artist { get; }
+
+    public Song(string songName, string artist)
+    {
+        SongName = songName;
+        Artist = artist;
+    }
+
+    public override string ToString()
+    {
+        return $"{SongName} - {Artist}";
+    }
+}
+
+
+public class PlayListQueue
+{
+    private LinkedList<Song> songs = new LinkedList<Song>();
+
+    public void AddSong(Song song)
+    {
+        songs.AddLast(song);
+    }
+
+    public Song PlayNext()   
+    {
+        if (songs.Count == 0)
+            return null;
+
+        Song current = songs.First.Value;
+        songs.RemoveFirst();
+        return current;
+    }
+
+    public int Count => songs.Count;   
+}
+public abstract class MediaPlayer
+{
+    public abstract void PlayNext();
+    public abstract void AddMedia(string title, string creator);
+}
+
+
+public class MusicPlayer : MediaPlayer
+{
+    private PlayListQueue playlist = new PlayListQueue();
+
+    public override void AddMedia(string title, string artist)
+    {
+        playlist.AddSong(new Song(title, artist));
+    }
+
+    public override void PlayNext()
+    {
+        Song song = playlist.PlayNext();
+
+        if (song == null)
+            Console.WriteLine("Playlist is empty");
+        else
+            Console.WriteLine($"Now Playing: {song}");
+    }
+}
+
