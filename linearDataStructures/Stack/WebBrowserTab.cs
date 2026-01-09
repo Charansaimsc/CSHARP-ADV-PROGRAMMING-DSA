78846a87386980422de122a4221a33f2b97e754d
Charansaimsc <charanmsc7969@gmail.com>
2026-01-09 12:46:30 +0530

    [charan] Add.Solution for Web Browser Tab Switching using Stack  problem

---
+++ linearDataStructures/Stack/WebBrowserTab.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+public interface INavigationManager
+{
+    void Navigate(string url);
+    void Back();
+    void Forward();
+    void Display();
+}
+public class Desktop : INavigationManager
+{
+    private Stack<string> backStack = new Stack<string>();
+    private Stack<string> forwardStack = new Stack<string>();
+    private string current;
+         public Desktop(string current)
+         {
+            this.current = current;
+            Console.WriteLine($"Opened:{current}");
+
+         }
+     public void Navigate(string url)
+{
+    backStack.Push(current);
+    current = url;
+    forwardStack.Clear();
+    Console.WriteLine($"Navigated to:{current}");
+
+}
+public void Back() 
+{ 
+    if(backStack.Count == 0)
+    {
+        return;
+    }
+    forwardStack.Push(current);
+    current = backStack.Pop();
+    Console.WriteLine($"BACK TO :{current}");
+}
+public void Forward()
+{
+    if(forwardStack.Count == 0)
+    {
+        return;
+    }
+    backStack.Push(current);
+    current = forwardStack.Pop();
+    Console.WriteLine($"Forward to: {current}");
+}
+public void Display()
+{
+    Console.WriteLine($"Current Page: {current}");
+}
+}
