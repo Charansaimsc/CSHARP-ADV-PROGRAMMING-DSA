da9f02471d7494d91709c30b9fcad78e594dc94e
Charansaimsc <charanmsc7969@gmail.com>
2026-01-09 15:42:23 +0530

    [charan] Add.Task Dispatcher problem using queue

---
+++ linearDataStructures/Queue/TaskDispatcher.cs
@@ -0,0 +1,56 @@
+﻿/*Use Case: Queue tasks like file uploads, analytics, or logs to background workers.
+OOP Concepts:
+● Interface: BackgroundTask
+● Encapsulation: TaskQueue shields queue logic.
+● Polymorphism: Different tasks (upload, report) use same interface.*/
+using System;
+using System.Collections.Generic;
+
+
+public interface IBackGroundTask
+{
+    void Execute();
+}
+public class Upload : IBackGroundTask
+{
+    public string FileName;
+    public Upload(string FileName)
+    {
+        this.FileName = FileName; 
+    }
+    public void Execute() 
+    {
+        Console.WriteLine($"FileUploading : {FileName}");
+    }
+}
+public class Report : IBackGroundTask
+{
+    public string FileName;
+    public Report(string FileName)
+    {
+        this.FileName = FileName;
+    }
+    public void Execute()
+    {
+        Console.WriteLine($"Getting Back WIth report : {FileName}");
+    }
+}
+public class PriorityTask
+{
+    private Queue<IBackGroundTask> queue = new Queue<IBackGroundTask>();
+    public void Add(IBackGroundTask task)
+    {
+        queue.Enqueue(task);
+        Console.WriteLine("Task added to list of tasks to be executed");
+    }
+    public void Next()
+    {
+        if (queue.Count == 0)
+        {
+            Console.WriteLine("no tasks in queue");
+            return;
+        }
+        IBackGroundTask task = queue.Dequeue();
+        task.Execute();
+    }
+}
