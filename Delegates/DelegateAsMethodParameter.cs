0fd13331aecf8811f83e75b54ecef6df7787397d
Charansaimsc <charanmsc7969@gmail.com>
2026-01-22 12:02:02 +0530

    [charan]Add. Solution if we pass delegate as parameter

---
+++ Delegates/DelegateAsMethodParameter.cs
@@ -0,0 +1,15 @@
+﻿using System;
+delegate int Operation(int a,int b);
+class Calculator
+{
+    public int Add(int a,int b) {  return a + b; }
+    public int Sub(int a,int b) {return a - b;}
+    public int Mul(int a, int b) { return a * b; }
+}
+class MathP
+{
+    public int PerformOperation(int a,int b,Operation Op)
+    {
+        return Op(a,b); 
+    }
+}
