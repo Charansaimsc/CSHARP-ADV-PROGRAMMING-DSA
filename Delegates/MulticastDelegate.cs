a133d7cf88726262ac6f58bbed852f1b3bf86ab3
Charansaimsc <charanmsc7969@gmail.com>
2026-01-22 12:16:13 +0530

    [charan]Add. Usage of multicast delegate

---
+++ Delegates/MulticastDelegate.cs
@@ -0,0 +1,15 @@
+﻿//Multicast Delegate
+
+//Create a delegate that returns void.
+
+//Attach multiple methods and observe the execution order.
+
+
+using System;
+
+class Calculationss
+{
+    public int Add(int a, int b) { return (a + b); }
+    public int Sub(int a, int b) { return (a - b); }
+    public int Mul(int a, int b) { return (a * b); }
+}
