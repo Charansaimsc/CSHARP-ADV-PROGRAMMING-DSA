1ed7f4584c4cee00adc1843311ceadf3ccc0d384
Charansaimsc <charanmsc7969@gmail.com>
2026-01-22 12:12:37 +0530

    [charan]Add. Solution Delegates with void return

---
+++ Delegates/DelegateWithVoid.cs
@@ -0,0 +1,31 @@
+﻿
+/*Delegate with void Return Type
+
+Define a delegate that prints a message.
+
+Pass different methods to display:
+
+
+Welcome message
+
+
+Error message
+
+
+Success message
+
+*/
+
+
+
+using System;
+delegate void Message(string message);
+class Messages
+{
+    public void Welcome(string message)
+    {
+        Console.WriteLine("welcome message");
+    }
+    public void Error(string message) { Console.WriteLine("errorrrrrrrr"); }
+    public void Success(string message) { Console.WriteLine("successssssssss"); }
+}
