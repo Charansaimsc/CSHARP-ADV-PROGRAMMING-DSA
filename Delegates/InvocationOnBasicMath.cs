cbae994aa1e5ed5f2dd9233d6d151e8e3c578cc6
Charansaimsc <charanmsc7969@gmail.com>
2026-01-22 12:15:36 +0530

    [charan]Add. Using Delegates ,Invocate on Some Basic Math operations

---
+++ Delegates/InvocationOnBasicMath.cs
@@ -0,0 +1,20 @@
+﻿/*Simple Delegate Invocation
+
+Create a delegate that takes two integers and returns an integer.
+
+Implement methods for Add, Subtract, Multiply, and invoke them using the delegate.*/
+
+
+using System;
+delegate int Math(int A,int B);
+class Calculation
+{
+    public static int  Add(int A, int B)
+    {
+        return A + B;
+    }
+    public static int Subtract(int A, int B) { return A - B; }
+    public static int Multiply(int A, int B) { return (A * B); }
+    public static int division(int A, int B) { return (A / B); }
+
+}
