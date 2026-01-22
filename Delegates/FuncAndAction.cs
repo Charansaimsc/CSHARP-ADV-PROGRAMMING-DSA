b85e2877741b5cb87f0bf37791498b10c5ddde53
Charansaimsc <charanmsc7969@gmail.com>
2026-01-22 12:14:07 +0530

    [charan]Add. InBuilt Func And Action

---
+++ Delegates/FuncAndAction.cs
@@ -0,0 +1,17 @@
+﻿/*Delegate vs Func/Action
+
+Rewrite a custom delegate using:
+
+
+Func<int, int, int>
+
+
+Action<string>*/
+
+using System;
+class Calculationsssss
+{
+    public void Add(int a, int b) { Console.WriteLine(a + b); }
+    public void Sub(int a, int b) { Console.WriteLine(a - b); }
+    public void Mul(int a, int b) { Console.WriteLine(a * b); }
+}
