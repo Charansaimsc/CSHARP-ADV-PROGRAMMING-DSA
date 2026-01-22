e6ad726a71fd909ff343b85f442e0257b92b900a
Charansaimsc <charanmsc7969@gmail.com>
2026-01-22 12:17:51 +0530

    [charan]Add. Main Method for all Delegate Problems

--- Delegates/Program.cs
+++ Delegates/Program.cs
@@ -1,2 +1,123 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+
+
+
+using System;
+using System.Runtime.InteropServices;
+namespace delegates
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            //Math math;
+            //math = Calculation.Add;
+            //Console.WriteLine(math(10, 14));
+            //math = Calculation.Subtract;
+            //Console.WriteLine(math(10, 14));
+            //math = Calculation.Multiply;
+            //Console.WriteLine(math(10, 14));
+
+
+
+            //Messages msg = new Messages();
+            //Message MS;
+            //MS = msg.Welcome;
+            //MS("charan");
+            //MS = msg.Error;
+            //MS("Invalid");
+            //MS = msg.Success;
+            //MS("sUCCESSFULL");
+
+
+            //Calculator C = new Calculator();
+            //MathP M = new MathP();
+
+            //int res1 = M.PerformOperation(90, 33, C.Add);
+            //Console.WriteLine(res1);
+            //int res2 = M.PerformOperation(88,44, C.Sub);
+            //Console.WriteLine(res2);
+            //int res3 = M.PerformOperation(90,2,C.Mul);
+            //Console.WriteLine(res3);
+
+
+            //Mathss MS;
+            //Calculations CS = new Calculations();
+            //MS = CS.Add;
+            //MS += CS.Sub;
+            //MS += CS.Mul;
+            //MS(20, 30);
+
+
+
+            //SquareDelegate SD = delegate (int n)
+            //{
+            //    return n * n;
+            //};
+            //int result = SD(9);
+            //Console.WriteLine(result);
+
+            // Calculationss CSS = new Calculationss();
+
+            // Func<int, int, int> fc = null;
+            // fc += CSS.Add;
+            // fc += CSS.Sub;
+            // fc += CSS.Mul;
+            //Console.WriteLine( fc(10, 44));
+
+            //Calculationsssss cxs = new Calculationsssss();
+            //Action<int, int> Act = null;
+            //Act += cxs.Add;
+            //Act += cxs.Sub;
+            //Act(90, 80);
+
+            //DLL dll = new DLL();
+            //dll.AddAtEnd(1);
+            //dll.AddAtEnd(53);
+            //dll.AddAtEnd(3);
+            //dll.AddAtEnd(47);
+            //dll.AddAtEnd(5);
+
+            //int target = 100;
+
+            //DLL result = new DLL();
+
+            //DLL.TargetSum(dll.Head, target, result);
+
+            //result.Print();
+
+
+
+            //String s1 = "listen";
+            //String s2 = "silent";
+            //char[] temp = s1.ToCharArray();
+            //int m = temp.Length; ;
+            //for (int i = 0; i < temp.Length; i++)
+            //{
+            //    if (s2.Contains(temp[i]))
+            //    {
+            //        m--;
+            //    }
+
+            //}
+            //if (m == 0)
+            //{
+            //    Console.WriteLine("Anagram");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("notaanagaram");
+            //}
+
+
+
+            //int[][] arr = new int[3][3];
+            //for(int i =0;)
+
+
+            //Generic G = new Generic();
+            Generic.Class<int>(29);
+
+        }
+    }
+}
